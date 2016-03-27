e6912262cae6a8e131d557136af1a3dbe7c521b0
Goryudyuma <umaretekyoumade@gmail.com>
2016-03-28 04:28:27 +0900

    other-s8pc-1A

---
+++ atcoder/other/s8pc-1/A/Main.java
@@ -0,0 +1,9 @@
+import java.util.*;
+
+class Main{
+    public static void main(String argv[]){
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        System.out.println((23 - 5) * 60.0 / --n);
+    }
+}
