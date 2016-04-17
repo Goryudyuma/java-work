36b2dc484765fe682a04ea76dc57c76e3151b9b1
Goryudyuma <umaretekyoumade@gmail.com>
2016-04-17 15:01:46 +0900

    other-code-thanks-festival-2015C

--- atcoder/other/code-thanks-festival/2015/C/Main.java
+++ atcoder/other/code-thanks-festival/2015/C/Main.java
@@ -4,20 +4,21 @@ class Main {
 	public static void main(String args[]) {
 		Scanner sc = new Scanner(System.in);
 		int N = sc.nextInt();
-		int ans;
-		switch (N) {
-			case 0:ans=32;break;
-			case 1:ans=1;break;
-			case 2:ans=6;break;
-			case 3:ans=0;break;
-			case 4:ans=2;break;
-			case 5:ans=4;break;
-			case 6:ans=7;break;
-			case 7:ans=13;break;
-			case 8:ans=11;break;
-			case 9:ans=5;break;
-			default :ans=0;
+		int H[] = new int[N];
+		for (int i = 0; i < N; i++) {
+			H[i] = sc.nextInt();
 		}
-		System.out.println(ans);
+		int b = -1;
+		int e = N;
+		int X = sc.nextInt();
+		while (b + 1 < e) {
+			int m = (b + e) / 2;
+			if (H[m] > X) {
+				e = m;
+			} else {
+				b = m;
+			}
+		}
+		System.out.println(b + 2);
 	}
 }
