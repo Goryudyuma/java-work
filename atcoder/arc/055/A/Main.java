ab65cbcd06732bae897a66f03573c7616e815da8
Goryudyuma <umaretekyoumade@gmail.com>
2016-06-04 11:00:54 +0900

    arc055A

---
+++ atcoder/arc/055/A/Main.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+class Main {
+	public static void main(String args[]) {
+		Scanner sc = new Scanner(System.in);
+		System.out.print("1");
+		int N = sc.nextInt() - 1;
+		for (int i = 0; i < N; i++) {
+			System.out.print("0");
+		}
+		System.out.println("7");
+	}
+}
