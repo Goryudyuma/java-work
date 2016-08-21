b60ecd6649148355c9859c5f8074f5333f407ae0
Goryudyuma <umaretekyoumade@gmail.com>
2016-08-21 11:30:32 +0900

    abc013C

--- atcoder/abc/013/C/Main.java
+++ atcoder/abc/013/C/Main.java
@@ -1,35 +1,28 @@
 import java.util.*;
- 
+
 class Main {
-        public static void main(String args[]) {
-                Scanner sc = new Scanner(System.in);
-                long N,H,A,B,C,D,E;
-                N=sc.nextInt();
-                H=sc.nextInt();
-                A=sc.nextInt();
-                B=sc.nextInt();
-                C=sc.nextInt();
-                D=sc.nextInt();
-                E=sc.nextInt();
-                long ans = 1 << 63 - 1;
-                for (int i = 0; i <= N; i++) {
-                        long q1 = i - 1 , q2 = N + 1;
-                        do{
-                                long j = (q1 + q2) / 2;
-                                long cost = 0;
-                                long man = H;
-                                cost = i * A + (j - i) * C;
-                                man  += i * B + (j - i) * D - (N - j) * E;
-                                if (man > 0) {
-                                        if ( ans > cost) {
-                                                ans = cost;
-                                        }
-                                        q2 = j;
-                                } else {
-                                        q1 = j ;
-                                }
-                        }while(q1 + 1< q2 );
-                }
-                System.out.println(ans);
-        }
+	public static void main(String args[]) {
+		Scanner sc = new Scanner(System.in);
+		long N,H,A,B,C,D,E;
+		N=sc.nextLong();
+		H=sc.nextLong();
+		A=sc.nextLong();
+		B=sc.nextLong();
+		C=sc.nextLong();
+		D=sc.nextLong();
+		E=sc.nextLong();
+		long ans = (1L << 63) - 1;
+		for (long i = 0; i <= N; i++) {
+			long j = (E * N - H - (B + E) * i);
+			if (j < 0) {
+				j = 0;
+			} else {
+				j = j / (D + E) + 1;
+			}
+			if (i + j <= N) {
+				ans = Math.min(ans, A * i + C * j);
+			}
+		}
+		System.out.println(ans);
+	}
 }
